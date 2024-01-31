f91b75903eb15fe7aa9281b99d8f07e10ba63a3a
sirfixalotgk <sirfixalot@woh.rr.com>
2024-01-31 11:01:51 +0000

    Repo updated to pull from custom source

--- moonraker/components/update_manager/base_config.py
+++ moonraker/components/update_manager/base_config.py
@@ -24,22 +24,22 @@ KLIPPER_DEFAULT_EXEC = os.path.expanduser("~/klippy-env/bin/python")
 
 BASE_CONFIG: Dict[str, Dict[str, str]] = {
     "moonraker": {
-        "origin": "https://github.com/arksine/moonraker.git",
+        "origin": "https://github.com/sirfixalotgk/QidiTechMoonraker.git",
         "requirements": "scripts/moonraker-requirements.txt",
         "venv_args": "-p python3",
         "install_script": "scripts/install-moonraker.sh",
-        "host_repo": "arksine/moonraker",
+        "host_repo": "sirfixalotgk/QidiTechMoonraker",
         "env": sys.executable,
         "path": MOONRAKER_PATH,
         "managed_services": "moonraker"
     },
     "klipper": {
-        "moved_origin": "https://github.com/kevinoconnor/klipper.git",
-        "origin": "https://github.com/Klipper3d/klipper.git",
+        "moved_origin": "https://github.com/sirfixalotgk/QidiTechKlipper.git",
+        "origin": "https://github.com/sirfixalotgk/QidiTechKlipper.git",
         "requirements": "scripts/klippy-requirements.txt",
         "venv_args": "-p python2",
         "install_script": "scripts/install-octopi.sh",
-        "host_repo": "arksine/moonraker",
+        "host_repo": "sirfixalotgk/QidiTechMoonraker",
         "managed_services": "klipper"
     }
 }
